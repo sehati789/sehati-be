30201632ea6b817d027179c7aac2b3dad9dba134
Vv-kK <veronicakylie1@gmail.com>
2024-06-28 10:46:00 +0700

    Initialization

---
+++ src/main/java/id/sehatibe/SehatiBeApplication.java
@@ -0,0 +1,13 @@
+package id.sehatibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SehatiBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SehatiBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/sehatibe/SehatiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package id.sehatibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SehatiBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
