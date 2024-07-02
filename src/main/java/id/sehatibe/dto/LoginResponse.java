00b508a496c31a0a70d6d8b8b571c033deaaf8e4
Naomi <gabriella.naomi21@ui.ac.id>
2024-07-02 19:35:40 +0700

    create controller for auth

---
+++ src/main/java/id/sehatibe/dto/LoginResponse.java
@@ -0,0 +1,14 @@
+package id.sehatibe.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginResponse {
+    private String token;
+
+    private long expiresIn;
+}
