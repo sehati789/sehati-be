7212b56bed79f45d76ce8145bb2648061f4e7b9b
Naomi <gabriella.naomi21@ui.ac.id>
2024-07-02 21:08:50 +0700

    fix error

--- src/main/java/id/sehatibe/SehatiBeApplication.java
+++ src/main/java/id/sehatibe/SehatiBeApplication.java
@@ -2,11 +2,8 @@ package id.sehatibe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = {"id.sehatibe.controller", "id.sehatibe.service", "id.sehatibe.repository"})
+@SpringBootApplication
 public class SehatiBeApplication {
 
     public static void main(String[] args) {
@@ -14,3 +11,4 @@ public class SehatiBeApplication {
     }
 
 }
+
