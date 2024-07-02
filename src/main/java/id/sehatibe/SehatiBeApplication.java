f2e738764f38c65dc2e1415632bc8b83271d240b
Vv-kK <veronicakylie1@gmail.com>
2024-07-02 17:03:39 +0700

    Fix config

--- src/main/java/id/sehatibe/SehatiBeApplication.java
+++ src/main/java/id/sehatibe/SehatiBeApplication.java
@@ -2,8 +2,11 @@ package id.sehatibe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan(basePackages = {"id.sehatibe.controller", "id.sehatibe.service", "id.sehatibe.repository"})
 public class SehatiBeApplication {
 
     public static void main(String[] args) {
